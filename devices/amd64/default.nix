881c4a1f2a87082383b4ab185cf91b4d08e7f3e2
RyzeNGrind <vidurshan.sribalasuhabiramam@ontariotechu.net>
2023-08-15 15:53:05 +0000

    sample flakes for amd64

---
+++ devices/amd64/default.nix
@@ -0,0 +1,28 @@
+{
+  description = "A simple flake for managing amd64 devices";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devos.url = "github:divnix/devos";
+    std.url = "github:divnix/std";
+    liminix.url = "github:liminix/liminix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devos, std, liminix }: {
+    nixosConfigurations = flake-utils.lib.eachDefaultSystem (system: {
+      inherit system;
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+        {
+          nixpkgs.overlays = [
+            (self: super: {
+              inherit (std) router;
+            })
+          ];
+        }
+      ];
+    });
+  };
+}
