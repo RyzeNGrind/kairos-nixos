475474386b351649d46df4a2cad07e5722271500
RyzeNGrind <vidurshan.sribalasuhabiramam@ontariotechu.net>
2023-08-16 06:57:16 +0000

    gha testing, ignore all but test & nixnas0 pseudo

---
+++ devices/arm64/network/nixnas0/default.nix
@@ -0,0 +1,7 @@
+#Build me a kairos enabled nix flake to initialize nixnas0 with NixOS configuration and netboot from either aif not already initiliazed.
+
+#The hardware modules will consist of Raspberry Pi 4B+ 8GB and Argon EON Pi Case.
+
+#A kairos-nixos-wildland compatible raid configuration to manage 4 x WD Pro NAS 20TB HDDs.
+
+#Allow toggling of cloud-infra/storage/default.nix flake to initCloudStorage.
